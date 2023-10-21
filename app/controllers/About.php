336bb76324d3ea30648ffeafe6ac2bf9dbb96156
unknown <193040038.ilham@mail.unpas.ac.id>
2023-10-22 01:00:16 +0700

    Controller MVC

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+class About{
+    public function index($nama = 'Ilham', $pekerjaan = 'Mahasiswa'){
+        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+
+    public function page(){
+        echo 'About/page';
+    }
+}
