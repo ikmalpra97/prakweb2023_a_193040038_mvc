7617ad3b166838e3f310802d0400c2d8e7701b5b
unknown <193040038.ilham@mail.unpas.ac.id>
2023-10-22 01:05:49 +0700

    views MVC

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,9 +3,22 @@
 class About{
     public function index($nama = 'Ilham', $pekerjaan = 'Mahasiswa'){
         echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+class About extends Controller{
+    public function index($nama = 'Ilham', $umur = 19, $pekerjaan = 'Mahasiswa'){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page(){
         echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
